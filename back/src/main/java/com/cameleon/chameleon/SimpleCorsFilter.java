3a855d4d2c74a6313be0558ea22a95f27dc5c361
Aurelien SCHILTZ <aschiltz@juniorisep.com>
2017-03-23 18:26:25 +0100

    [Back] Handle CORS

---
+++ back/src/main/java/com/cameleon/chameleon/SimpleCorsFilter.java
@@ -0,0 +1,47 @@
+package com.cameleon.chameleon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+    @Autowired
+    private Environment env;
+
+    public SimpleCorsFilter() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", env.getProperty("cors.allowed_origin"));
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- back/src/main/java/com/cameleon/chameleon/controller/HomeController.java
+++ back/src/main/java/com/cameleon/chameleon/controller/HomeController.java
@@ -1,14 +1,20 @@
 package com.cameleon.chameleon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private Environment env;
 
     @GetMapping("/home")
     @ResponseBody
     public String homeController() {
-        return "<h1>Chameleon web service !</h1><h2>Now with automatic deployment !</h2>";
+        return "<h1>Chameleon web service !</h1>" +
+                "<h2>Now with automatic deployment !</h2>" +
+                "<p>CORS allowed origin : <tt>" + env.getProperty("cors.allowed_origin") + "</tt></p>";
     }
 }
