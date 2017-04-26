4f4a47af212fa9b559041d03c918517728cc38ac
victorely <vely@juniorisep.com>
2017-04-26 13:51:14 +0200

    Add TabsLayout

--- front/src/pages/client/index.js
+++ front/src/pages/client/index.js
@@ -1,110 +1,29 @@
 // @flow
 
 import React, { Component } from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { createStyleSheet } from 'jss-theme-reactor';
-import customPropTypes from 'material-ui/utils/customPropTypes';
-import Paper from 'material-ui/Paper';
-import { Tabs, Tab } from 'material-ui/Tabs';
-import PhoneIcon from 'material-ui-icons/Phone';
-import FavoriteIcon from 'material-ui-icons/Favorite';
-import PersonPinIcon from 'material-ui-icons/PersonPin';
-import HelpIcon from 'material-ui-icons/Help';
-import ShoppingBasket from 'material-ui-icons/ShoppingBasket';
-import ThumbDown from 'material-ui-icons/ThumbDown';
-import ThumbUp from 'material-ui-icons/ThumbUp';
 
-import colors from '../../colors.js';
+import TabsLayout from '../../components/TabsLayout';
 
-// pages
+// tabs :
 import Equipe from './equipe';
 import Sujet from './sujet';
-
-const styleSheet = createStyleSheet('ClientPage', () => ({
-  root: {
-    width: '100%',
-  },
-  appBar: {
-    backgroundColor: colors.ISEP_PRIMARY_LIGHTER,
-  },
-  white: {
-    color: colors.ISEP_TERTIARY,
-  },
-}));
 
 const tabs = [
   {
     label: "Sujet",
     path: "/subject",
-    icon: PhoneIcon,
+    component: Sujet,
   },
   {
     label: "Équipe",
     path: "/team",
-    icon: FavoriteIcon,
+    component: Equipe,
   },
 ];
 
-export default class ClientPage extends Component {
-  state = {
-    index: 0,
-  };
-
-  componentDidMount() {
-    this.transitionTo(tabs[this.state.index].path); // TODO with a redirect exact plz
-  }
-
-  handleChange = (event, index) => {
-    this.transitionTo(tabs[index].path);
-    this.setState({ index });
-  };
-
-  transitionTo = (path) => {
-    const {url} = this.props.match;
-    this.props.history.push(url + path); // TODO urljoin
-  };
-
-  render() {
-    const {match} = this.props;
-    const classes = this.context.styleManager.render(styleSheet);
-
-    return (
-      <div>
-        <Paper className={classes.root}>
-          <div className={classes.appBar}>
-            <Tabs
-              indicatorColor={ colors.ISEP_SECONDARY }
-              index={this.state.index}
-              onChange={this.handleChange}
-              scrollable
-              scrollButtons="on"
-              textColor="accent">
-              {
-                tabs.map((tab, index) => {
-                  const { label, icon: Icon } = tab;
-                  return (
-                    <Tab
-                      key={index}
-                      className={classes.white}
-                      label={label}
-                      icon={<Icon />} />
-                  );
-                })
-              }
-            </Tabs>
-          </div>
-        </Paper>
-
-        <Switch>
-          <Route path={`${match.url}/subject`} component={Sujet} />
-          <Route path={`${match.url}/team`} component={Equipe} />
-        </Switch>
-      </div>
-    );
-  }
+export default function TeacherPage(props) {
+  const baseLocation = props.match.url;
+  return (
+    <TabsLayout tabs={tabs} baseLocation={baseLocation} />
+  );
 }
-
-ClientPage.contextTypes = {
-  styleManager: customPropTypes.muiRequired,
-};
--- front/src/pages/teacher/index.js
+++ front/src/pages/teacher/index.js
@@ -1,83 +1,35 @@
 // @flow
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import {createStyleSheet} from 'jss-theme-reactor';
-import customPropTypes from 'material-ui/utils/customPropTypes';
-import {BottomNavigation, BottomNavigationButton} from 'material-ui/BottomNavigation';
-import RestoreIcon from 'material-ui-icons/Restore';
-import FavoriteIcon from 'material-ui-icons/Favorite';
+import React, { Component } from 'react';
 
-// pages
+import TabsLayout from '../../components/TabsLayout';
+
+// tabs :
 import Equipe from './equipe';
 import Sujet from './sujet';
-import NewPromo from './new-promo';
 import Promo from './promo';
 
-const styleSheet = createStyleSheet('BottomNavigation', () => ({
-  root: {
-    textAlign: 'center',
+const tabs = [
+  {
+    label: "Sujet",
+    path: "/subject",
+    component: Sujet,
   },
-}));
+  {
+    label: "Équipe",
+    path: "/team",
+    component: Equipe,
+  },
+  {
+    label: "Promotion",
+    path: "/promo",
+    component: Promo,
+  },
+];
 
-export default class Teacher extends React.Component {
-  state = {
-    index: 0,
-  };
-
-  handleChange = (event, index) => {
-    this.setState({
-      index
-    });
-  };
-
-  // Returns an event handler :
-  transitionTo = (path) => {
-    return () => {
-      const {url} = this.props.match;
-      this.props.history.push(url + path); // TODO urljoin
-    }
-  }
-
-  render() {
-    const { match } = this.props;
-    const { index } = this.state;
-    const classes = this.context.styleManager.render(styleSheet);
-
-
-    return (
-      <div>
-        <div className={classes.root}>
-          <BottomNavigation
-            index={index}
-            onChange={this.handleChange}
-            showLabels>
-            <BottomNavigationButton
-              label="Sujets"
-              onClick={this.transitionTo("/subject")}
-              icon={<RestoreIcon />}
-            />
-            <BottomNavigationButton
-              label="Équipes"
-              onClick={this.transitionTo("/team")}
-              icon={<FavoriteIcon />} />
-            <BottomNavigationButton
-              label="Promo"
-              onClick={this.transitionTo("/promo")}
-              icon={<FavoriteIcon />} />
-          </BottomNavigation>
-        </div>
-        <Switch>
-          <Route path={`${match.url}/subject`} component={Sujet} />
-          <Route path={`${match.url}/team`} component={Equipe} />
-          <Route path={`${match.url}/promo`} component={Promo} />
-          <Route path={`${match.url}/new-promo`} component={NewPromo} />
-        </Switch>
-      </div>
-    );
-  }
+export default function TeacherPage(props) {
+  const baseLocation = props.match.url;
+  return (
+    <TabsLayout tabs={tabs} baseLocation={baseLocation} />
+  );
 }
-
-Teacher.contextTypes = {
-  styleManager: customPropTypes.muiRequired,
-};
