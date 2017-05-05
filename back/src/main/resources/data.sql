2ea8674e9fbd992b1aed64de613ce381dc3cb33d
Aurelien SCHILTZ <aschiltz@juniorisep.com>
2017-05-05 16:19:48 +0200

    [Default data] client / student / teacher with passwords : "test"

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -1,7 +1,25 @@
 -- default roles
-# INSERT INTO `role` (`authority`) VALUES ('ROLE_STUDENT');
-# INSERT INTO `role` (`authority`) VALUES ('ROLE_TEACHER');
-# INSERT INTO `role` (`authority`) VALUES ('ROLE_CLIENT');
+INSERT INTO `role` (`authority`) VALUES ('ROLE_STUDENT');
+INSERT INTO `role` (`authority`) VALUES ('ROLE_TEACHER');
+INSERT INTO `role` (`authority`) VALUES ('ROLE_CLIENT');
 
--- default user
-# INSERT INTO `user` (username, password) VALUES ('user', '-90-127-117-127-120-7710868-47119-124-59853199-84-5293-22-8120111-9910-47-8260-40-40-121-53-8579119114-122-37-7721-520-12376-121116-361316-7586126741128554-52422997-201022-89-90');
+-- default users
+-- encrypted password = test
+INSERT INTO `user` (username, password) VALUES ('student', '3046-97-6204304511725-117117333125-95-70-846996-11110660109-109-68-505880-41-1615-45-107-652271-71-85-72-47-81-52-100118-62-119-80-555659-93-122-8786-387556-1096823120-98');
+INSERT INTO `user` (username, password) VALUES ('teacher', '3046-97-6204304511725-117117333125-95-70-846996-11110660109-109-68-505880-41-1615-45-107-652271-71-85-72-47-81-52-100118-62-119-80-555659-93-122-8786-387556-1096823120-98');
+INSERT INTO `user` (username, password) VALUES ('client', '3046-97-6204304511725-117117333125-95-70-846996-11110660109-109-68-505880-41-1615-45-107-652271-71-85-72-47-81-52-100118-62-119-80-555659-93-122-8786-387556-1096823120-98');
+
+INSERT INTO `user_roles` (user_id, roles_id) VALUES (
+  (SELECT id from user where username='student'),
+  (SELECT id from role where authority='ROLE_STUDENT')
+);
+
+INSERT INTO `user_roles` (user_id, roles_id) VALUES (
+  (SELECT id from user where username='teacher'),
+  (SELECT id from role where authority='ROLE_TEACHER')
+);
+
+INSERT INTO `user_roles` (user_id, roles_id) VALUES (
+  (SELECT id from user where username='client'),
+  (SELECT id from role where authority='ROLE_CLIENT')
+);
