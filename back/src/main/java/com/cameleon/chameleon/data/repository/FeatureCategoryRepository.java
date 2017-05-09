31234bac4aa1c3fac96fa456063005e5ade7f9c4
Aurelien SCHILTZ <aschiltz@juniorisep.com>
2017-05-09 15:21:15 +0200

    make the build pass

--- back/src/main/java/com/cameleon/chameleon/data/repository/FeatureCategoryRepository.java
+++ back/src/main/java/com/cameleon/chameleon/data/repository/FeatureCategoryRepository.java
@@ -3,5 +3,5 @@ package com.cameleon.chameleon.data.repository;
 import com.cameleon.chameleon.data.entity.Feature;
 import org.springframework.data.repository.CrudRepository;
 
-public class FeatureCategoryRepository extends CrudRepository<Feature, Long> {
+public interface FeatureCategoryRepository extends CrudRepository<Feature, Long> {
 }
