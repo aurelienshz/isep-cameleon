9db9f8b3ebbcbe72f0aa463a78e7a61cc39eb4ee
victorely <vely@juniorisep.com>
2017-06-01 15:24:33 +0200

    Improve React

--- front/src/index.js
+++ front/src/index.js
@@ -7,6 +7,7 @@ import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-r
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
+import registerServiceWorker from './registerServiceWorker';
 
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
@@ -54,3 +55,4 @@ ReactDOM.render(
   <DDApp />,
   document.getElementById('root')
 );
+registerServiceWorker();
