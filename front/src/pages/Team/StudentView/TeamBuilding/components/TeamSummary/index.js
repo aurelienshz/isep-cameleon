cd279a4bee9d9dcdc372945e1b897fe8fbae3467
victorely <vely@juniorisep.com>
2017-06-06 17:42:07 +0200

    Improve TeamSummary

--- front/src/pages/Team/StudentView/TeamBuilding/components/TeamSummary/index.js
+++ front/src/pages/Team/StudentView/TeamBuilding/components/TeamSummary/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 
+import Button from 'material-ui/Button';
+import List, { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+
 export default class TeamSummary extends React.Component {
   render() {
     const { team, leaveTeam } = this.props;
     return (
       <div>
         Équipe rejointe : {team.name}&nbsp;
-        <button onClick={() => leaveTeam(team.id)}>Quitter l'équipe</button>
+        <Button raised onClick={() => leaveTeam(team.id)}>Quitter l'équipe</Button>
         <hr/>
         Membres:
-        <ul>
-        {
-          team.members.map(m => (
-            <li key={m.id}>{m.firstName} {m.lastName} - <button>Accepter</button> / <button>Refuser</button> (boutons inactifs)</li>
-          ))
-        }
-        </ul>
+            <List>
+              {
+                team.members.map(m => (
+              <ListItem dense button key={m.id}>
+                <Avatar alt="Remy Sharp" src="/img/ely.jpg" />
+                <ListItemText primary={`${m.firstName} ${m.lastName}`}>{m.firstName}</ListItemText>
+                <ListItemSecondaryAction>
+                  <Button raised>Accepter</Button>
+                  <Button raised>refuser</Button>
+                  (boutons inactifs)
+                </ListItemSecondaryAction>
+              </ListItem>
+              ))
+            }
+            </List>
       </div>
     );
   }
