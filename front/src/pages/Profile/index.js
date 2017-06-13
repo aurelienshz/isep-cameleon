bf358e4a57eacf3c4d5edd842ad282307a8b023b
Victor Ely <vely@juniorisep.com>
2017-06-14 00:44:12 +0200

    Update profil

--- front/src/pages/Profile/index.js
+++ front/src/pages/Profile/index.js
@@ -4,15 +4,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import Avatar from 'material-ui/Avatar';
 import Button from 'material-ui/Button';
-import Dialog from 'material-ui/Dialog';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import IconButton from 'material-ui/IconButton';
-import Typography from 'material-ui/Typography';
-import CloseIcon from 'material-ui-icons/Close';
-import Slide from 'material-ui/transitions/Slide';
-import TextField from 'material-ui/TextField';
-import InfiniteCalendar from 'react-infinite-calendar';
 import Loader from '../../components/Loader.js';
 import 'react-infinite-calendar/styles.css';
 
@@ -76,39 +67,6 @@ const STYLE_INFO_TABLE_TH = {
   paddingRight: '10px',
 };
 
-const STYLE_BUTTON_MODAL = {
-  display: 'inline-block',
-  verticalAlign: 'bottom',
-  fontSize: '13px',
-  padding: '5px',
-  float: 'right',
-  margin: 0,
-  fontWeight: 'bold',
-  backgroundColor: colors.ISEP_SECONDARY,
-  color: colors.ISEP_TERTIARY,
-};
-
-const STYLE_APPBAR = {
-  position: 'relative',
-  backgroundColor: colors.ISEP_PRIMARY,
-};
-
-const STYLE_FLEX = {
-  flex: 1,
-};
-
-const STYLE_INPUT = {
-  margin: 20,
-};
-
-const STYLE_BUTTON_ONMODAL = {
-  width: '10%',
-  marginLeft: '45%',
-  backgroundColor: colors.ISEP_SECONDARY,
-  color: colors.ISEP_TERTIARY,
-};
-
-
 class Profil extends React.Component {
 
   state = {
@@ -137,7 +95,7 @@ class Profil extends React.Component {
 
   isProfileLoaded = () => {
     const profile = this.props.profile;
-    return Boolean(profile) && Boolean(profile.firstName) && Boolean(profile.lastName);
+    return Boolean(profile) && Boolean(profile.firstName) && Boolean(profile.lastName) && Boolean(profile.mail) && Boolean(profile.isepNumber);
   };
 
   logout = () => {
@@ -152,8 +110,13 @@ class Profil extends React.Component {
         <h1>Profil</h1>
         <div style={STYLE_PROFILE_HEADER}>
           <Avatar
-            alt="Victor ELY"
-            src="/img/ely.jpg"
+            alt="Photo de profil"
+            src={
+              this.isProfileLoaded() && !awaitingProfile ?
+              `http://storage.iseplive.fr/avatars/95/`+profile.isepNumber+`.jpg`
+              :
+              <Loader />
+            }
             style={STYLE_AVATAR}
           />
           <div>
@@ -163,11 +126,21 @@ class Profil extends React.Component {
         </div>
         <div style={STYLE_SEC}>
           <h2 style={STYLE_TITLE_CAPS}>Mes informations</h2>
-          <Button onClick={this.handleOpen} style={STYLE_BUTTON_MODAL}>
-          Éditer
-          </Button>
           <table style={STYLE_INFO_TABLE}>
             <tbody>
+              <tr style={STYLE_INFO_TABLE_TR}>
+                <th style={STYLE_INFO_TABLE_TH}>
+                  Numéro étudiant
+                </th>
+                <td>
+                  {
+                    this.isProfileLoaded() && !awaitingProfile ?
+                    profile.isepNumber
+                    :
+                    <Loader />
+                  }
+                </td>
+              </tr>
               <tr style={STYLE_INFO_TABLE_TR}>
                 <th style={STYLE_INFO_TABLE_TH}>
                   Nom d'utilisateur
@@ -186,75 +159,17 @@ class Profil extends React.Component {
                   Email
                 </th>
                 <td>
-                  victor.ely@orange.fr
-                </td>
-              </tr>
-              <tr style={STYLE_INFO_TABLE_TR}>
-                <th style={STYLE_INFO_TABLE_TH}>
-                  Date de naissance
-                </th>
-                <td>
-                  28/01/2017
-                </td>
-              </tr>
-              <tr style={STYLE_INFO_TABLE_TR}>
-                <th style={STYLE_INFO_TABLE_TH}>
-                  Adresse
-                </th>
-                <td>
-                  9, boulevard de Dixmude 75017 PARIS
-                </td>
-              </tr>
-              <tr style={STYLE_INFO_TABLE_TR}>
-                <th style={STYLE_INFO_TABLE_TH}>
-                  Tel
-                </th>
-                <td>
-                  0670767380
-                </td>
-              </tr>
-              <tr style={STYLE_INFO_TABLE_TR}>
-                <th style={STYLE_INFO_TABLE_TH}>
-                  Promotion
-                </th>
-                <td>
-                  2018
+                  {
+                    this.isProfileLoaded() && !awaitingProfile ?
+                    profile.mail
+                    :
+                    <Loader />
+                  }
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
-        <div style={STYLE_SEC}>
-          <h2 style={STYLE_TITLE_CAPS}>Mes données</h2>
-        </div>
-        <table style={STYLE_INFO_TABLE}>
-          <tbody>
-            <tr style={STYLE_INFO_TABLE_TR}>
-              <th style={STYLE_INFO_TABLE_TH}>
-                Nom du groupe
-              </th>
-              <td>
-                Test
-              </td>
-            </tr>
-            <tr style={STYLE_INFO_TABLE_TR}>
-              <th style={STYLE_INFO_TABLE_TH}>
-                Nom du sujet
-              </th>
-              <td>
-                Coucou
-              </td>
-            </tr>
-            <tr style={STYLE_INFO_TABLE_TR}>
-              <th style={STYLE_INFO_TABLE_TH}>
-                Avancement
-              </th>
-              <td>
-                Phase -1
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     );
   }
